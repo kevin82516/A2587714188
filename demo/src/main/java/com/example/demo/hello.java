73942d1e97ac2cc47b4361e3e5134f81e272911e
kevin <kevin@laanzhuodeMBP.lan>
2020-03-13 16:42:51 +0800

    測試有格式的字串與Get的使用

--- demo/src/main/java/com/example/demo/hello.java
+++ demo/src/main/java/com/example/demo/hello.java
@@ -1,11 +1,10 @@
 package com.example.demo;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 class hello {
@@ -18,8 +17,40 @@ class hello {
         return "test";
     }
 }
-@Controller
-class testdemo{
-    @RequestMapping("/demo")
-    public String ttt(){return "test25";}
+
+@RestController
+class MemberApiController {
+    @Autowired
+    MemberAccount memberAccount;
+    @RequestMapping("/memberApi/memberTest")
+    public MemberAccount memberTest() {
+        MemberAccount memberAccount = new MemberAccount();
+        memberAccount.setAddress("台北市");
+        memberAccount.setCellphone("09123456789");
+        memberAccount.setEmail("test@gmail.com");
+        memberAccount.setId(1);
+        memberAccount.setPassword("123456789");
+        return memberAccount;
+    }
 }
+@RestController
+class productControll{
+@GetMapping(value = "/products/{id}")
+    public String getProduct(@PathVariable("id") String id) {
+        Product product = new Product();
+        product.setId(id);
+        return "Horror Movie Episode " + id;
+    }
+}
+
+class Product{
+    String id;
+    String setId(String id){
+        this.id=id;
+        return id;
+    }
+}
+
+
+
+
