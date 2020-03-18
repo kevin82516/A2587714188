8c1636600db66b24312e0b2c695cea099d13b5c5
kevin <kevin@laanzhuodeMBP.lan>
2020-03-18 11:07:03 +0800

    建立CRUD頁面與連結

--- demo/src/main/java/com/example/demo/mysqldemo/sqlControll.java
+++ demo/src/main/java/com/example/demo/mysqldemo/sqlControll.java
@@ -4,12 +4,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
+@RequestMapping("/mysql")
 class mysqldemo {
-    @ResponseBody
-    @RequestMapping("/mysql")
-    public String mysql() {
+    @RequestMapping(value = "home")
+    public String mysqlhome() {
+        String db;
         DBConnect connection=new DBConnect();
-        return String.valueOf(connection.getData());
+        db=String.valueOf(connection.getData());
+        return "mysqlhome";
+    }
+
+    @RequestMapping(value = "Create")
+    public String mysqlCreate() {
+        return "mysqlCreate";
+    }
+
+    @RequestMapping(value = "Add")
+    public String mysqlAdd() {
+        return "mysqlAdd";
+    }
+
+    @RequestMapping(value = "Updata")
+    public String mysqlUpdata() {
+        return "mysqlUpdata";
+    }
+
+    @RequestMapping(value = "Delet")
+    public String mysqlDelet() {
+        return "mysqlDelet";
     }
 }
